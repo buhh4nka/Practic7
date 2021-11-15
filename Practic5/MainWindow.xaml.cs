b422d26bffa02c285e500c446cc7669b365c2b69
Nazarov Dima <dmitriynazarov90@gmail.com>
2021-11-15 19:12:44 +0300

    Добавьте файлы проекта.

---
+++ Practic5/MainWindow.xaml.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Practic5
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+
+    public partial class MainWindow : Window
+    {
+        Pair _firstPair = new Pair();
+        Pair _secondPair = new Pair();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void help_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("ИСП-31. Назаров Д. Вариант 2. \nСоздать класс Pair (пара чисел). Создать необходимые методы и свойства. Определить методы метод сложения полей и операцию сложения пар(а, b) + (с, d)= (а + c, b + d).\nСоздать перегруженные методы для увеличения полей на 1, сложения трех пар чисел.", "О программе");
+        }
+
+        private void exit_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void startSum_Click(object sender, RoutedEventArgs e)
+        {
+            bool isNotErrorInFirstPair1 = Int32.TryParse(firstPairFirstNumber.Text, out int firstNumberPair1);
+            bool isNotErrorInSecondPair1 = Int32.TryParse(firstPairSecondNumber.Text, out int secondNumberPair1);
+            bool isNotErrorInFirstPair2 = Int32.TryParse(secondPairFirstNumber.Text, out int firstNumberPair2);
+            bool isNotErrorInSecondPair2 = Int32.TryParse(secondPairSecondNumber.Text, out int secondNumberPair2);
+            if (isNotErrorInFirstPair1 && isNotErrorInFirstPair2 && isNotErrorInSecondPair1 && isNotErrorInSecondPair2)
+            {
+                _firstPair.First = firstNumberPair1;
+                _firstPair.Second = secondNumberPair1;
+                _secondPair.First = firstNumberPair2;
+                _secondPair.Second = secondNumberPair2;
+                Pair endPair = new Pair();
+                endPair = _firstPair + _secondPair;
+                endSum.Text = $"{endPair.First}, {endPair.Second}";
+            }
+            else
+            {
+                MessageBox.Show("Введены некоректные значения, введите другие", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+                firstPairFirstNumber.Clear();
+                firstPairSecondNumber.Clear();
+                secondPairFirstNumber.Clear();
+                secondPairSecondNumber.Clear();
+                endSum.Clear();
+            }
+        }
+
+        private void addNumber_Click(object sender, RoutedEventArgs e)
+        {
+            bool isNotErrorInNumber = double.TryParse(numberToAdd.Text, out double number);
+            if (isNotErrorInNumber)
+            {
+                Money endMoney = new Money(number);
+                if (endMoney.Second < 10) endSum.Text = $"{endMoney.First}, 0{endMoney.Second}";
+                else endSum.Text = $"{endMoney.First}, {endMoney.Second}";
+
+
+
+            }
+            else
+            {
+                MessageBox.Show("Введены некоректные значения, введите другие", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+                numberToAdd.Clear();
+            }
+        }
+
+
+    }
+}
---
+++ Practic5/Pair.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Practic5
+{
+    class Pair
+    {
+
+        public Pair()
+        {
+            First = 0;
+            Second = 0;
+        }
+        public Pair(int first, int second)
+        {
+            First = first;
+            Second = second;
+        }
+        public int First { get; set; }
+        public int Second { get; set; }
+        public static Pair operator +(Pair firstPair, Pair secondPair)
+        {
+            Pair result = new Pair();
+            result.First = firstPair.First + secondPair.First;
+            result.Second = firstPair.Second + secondPair.Second;
+            return result;
+        }
+        public static Pair operator +(Pair firstPair, int number)
+        {
+            Pair result = new Pair();
+            result.First = firstPair.First + number;
+            result.Second = firstPair.Second + number;
+            return result;
+        }
+
+        public static Pair operator ++(Pair pair)
+        {
+            pair.First += 1;
+            pair.Second += 1;
+            return pair;
+        }
+        public static Pair operator --(Pair pair)
+        {
+            pair.First -= 1;
+            pair.Second -= 1;
+            return pair;
+        }
+    }
+    class Money : Pair
+    {
+        public Money()
+        {
+            First = 0;
+            Second = 0;
+        }
+        public Money(int ruble, int kopeck) : base(ruble, kopeck)
+        {
+        }
+        public Money(double money)
+        {
+            First = (int)money;
+            Second = Convert.ToInt32((money - (int)money) * 100);
+        }
+        public static Money operator +(Money firstNumber, Money secondNumber)
+        {
+            Money result = new Money();
+            result.First = firstNumber.First + secondNumber.First;
+            result.Second = firstNumber.Second + secondNumber.Second;
+            while (result.Second >= 100)
+            {
+                result.Second -= 100;
+                result.First += 1;
+            }
+            return result;
+        }
+        public Money Minus(ref Money money, int ruble, int kopeck)
+        {
+            money.First -= ruble;
+            money.Second -= kopeck;
+            while (money.Second < 0)
+            {
+                money.First -= 1;
+                money.Second += 100;
+            }
+            return money;
+        }
+
+    }
+}
